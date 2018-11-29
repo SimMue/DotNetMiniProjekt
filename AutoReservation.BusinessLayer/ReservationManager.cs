a60eb4eccf6125eba759765c9476843e4b9ceadc
Raphael Rechsteiner <raphael.rechsteiner@gmail.com>
2018-11-29 17:04:46 +0100

    Add Kunde/ReservationManager

--- AutoReservation.BusinessLayer/ReservationManager.cs
+++ AutoReservation.BusinessLayer/ReservationManager.cs
@@ -1,19 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoReservation.Dal;
 using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoReservation.BusinessLayer
 {
     public class ReservationManager
         : ManagerBase
     {
-        public Reservation GetById(int v)
+        public List<Reservation> List
         {
-            throw new NotImplementedException();
+            get
+            {
+                using (AutoReservationContext context = new AutoReservationContext())
+                {
+                    return context.Reservationen.ToList();
+                }
+            }
+        }
+
+        public Reservation GetById(int id)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return context.Reservationen.ToList().First(r => r.ReservationsNr == id);
+            }
+        }
+
+        public void Add(Reservation reservation)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                // Insert
+                context.Entry(reservation).State = EntityState.Added;
+                context.SaveChanges();
+            }
+
         }
 
         public void Update(Reservation reservation)
         {
-            throw new NotImplementedException();
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                // Update
+                context.Entry(reservation).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Delete(Reservation reservation)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                // Delete
+                context.Entry(reservation).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+
         }
     }
 }
