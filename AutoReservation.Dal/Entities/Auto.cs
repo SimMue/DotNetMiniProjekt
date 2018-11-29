88f46597ee6b23affc691dde676b9e32b599c691
s6muelle <s6muelle@PIN1258028>
2018-11-29 14:31:20 +0100

    model init

--- AutoReservation.Dal/Entities/Auto.cs
+++ AutoReservation.Dal/Entities/Auto.cs
@@ -1,4 +1,27 @@
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AutoReservation.Dal.Entities
 {
+    public abstract class Auto
+    {
+        public int Id { get; set; }
 
+        public string Marke { get; set; }
+
+        public int Tagestarif { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+
+    public class StandardAuto : Auto { }
+
+    public class LuxusklasseAuto : Auto
+    {
+        public int Basistarif { get; set; }
+    }
+
+    public class MittelklasseAuto : Auto { }
 }
--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -1,4 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AutoReservation.Dal.Entities
 {
+    public class Reservation
+    {
+        public int ReservationsNr { get; set; }
 
+        public Auto AutoId { get; set; }
+
+        public Kunde KundenId { get; set; }
+
+        public DateTime Von { get; set; }
+
+        public DateTime Bis { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
 }
